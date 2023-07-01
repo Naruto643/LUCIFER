8211293716ba4ebdae7b14a4109c91fd28c3c43c
AS TG[ONLINE] <126592884+TGxIRFAN@users.noreply.github.com>
2023-07-01 14:48:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,6 @@ logging.getLogger("imdbpy").setLevel(logging.ERROR)
 
 from pyrogram import Client, __version__, filters
 from pyrogram.raw.all import layer
-from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR, LOG_CHANNEL, PORT
 from utils import temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
